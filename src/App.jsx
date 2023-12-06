3f12bb4578829bd50ceff69fdebb919a3fdc5e4c
muritadb <douglasb008@gmail.com>
2023-12-06 12:42:44 +0000

    starter studies

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const AppStyled = styled.div`
+background-color: #555;
+color: #fff;
+
+`
+
+function App() {
+  return <AppStyled>ola mundo</AppStyled>}
+
+export default App
